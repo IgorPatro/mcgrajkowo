d306f33361fbb6d6f99bdca3c22cdc5579fdc062
Igor Patro <i.patro@wp.pl>
2020-05-18 10:36:44 +0200

    Form finished, pay modal created and function to changing values created too, fixed some bugs

--- js/functions.js
+++ js/functions.js
@@ -19,3 +19,38 @@ function hide(what, time) {
         what.style.visibility = 'hidden';
     }, time * 100 + 300);
 }
+
+
+
+// PAY MODAL SHOW AND CHANGE VALUES
+function showPayModal(icon, name, value) {
+    let packageIconContainer = document.querySelector('.pay_tile i'),
+        packageNameContainer = document.querySelector('.pay_tile h3.offer'),
+        packageValueContainer = document.querySelector('.pay_tile h2.offer');
+
+    let nickInput = document.querySelector('.pay_form input.pay_nick'),
+        emailInput = document.querySelector('.pay_form input.pay_email'),
+        infoValueContainer = document.querySelector('.pay_informations span.toPay'),
+        infoNickContainer = document.querySelector('.pay_informations span.toNick');
+
+    // Tile values changing
+    packageIconContainer.classList = `fas ${icon}`;
+    packageNameContainer.textContent = name;
+    packageValueContainer.textContent = value;
+
+    // Value in info section changing
+    infoValueContainer.textContent = value;
+
+    // changing nick
+    nickInput.addEventListener('change', function () {
+        infoNickContainer.textContent = nickInput.value;
+    });
+
+    // Show modal function
+    show(payModal, 0.5);
+}
+
+
+
+
+// PAYING FUNCTION AND VALIDATION
---
+++ js/index.js
@@ -0,0 +1,24 @@
+// Pay modal consts
+const payModal = document.querySelector('section.pay_modal');
+const payModalOn = document.querySelectorAll('button.offer-btn');
+const payModalOff = document.querySelector('button.close_pay');
+const payButton = document.querySelector('button.payAccept');
+
+// Turning pay modal on
+payModalOn.forEach(button => {
+    button.addEventListener('click', function () {
+        // icon, name, value
+        icon = this.dataset.icon;
+        name = this.dataset.name;
+        value = this.dataset.value;
+
+        // Giving params to function
+        showPayModal(icon, name, value);
+    });
+});
+
+
+// Turning pay modal off
+payModalOff.addEventListener('click', function () {
+    hide(payModal, 0.5);
+});
